b8d0fb7377f1693ea2632a2d3c5d79611ad26b7c
johandahlberg <jd222qd@lnu.student.se>
2019-02-22 15:12:47 +0100

    Iteration 2.

--- src/hangman_project/HangmanMain.java
+++ src/hangman_project/HangmanMain.java
@@ -1,12 +1,10 @@
 package hangman_project;
 
-import java.util.Scanner;
-
 public class HangmanMain {
 	
 	public static void main(String[] args) throws InterruptedException
 	{
 		HangmanLogic.welcomeText();
-	}
+	}	
 
 }
--- src/hangman_project/ReadWords.java
+++ src/hangman_project/ReadWords.java
@@ -1,10 +1,29 @@
 package hangman_project;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Random;
+
 public class ReadWords {
 	
 	public static String getRandomWord()
 	{
-		return null;
+		String word = "";
+		try {
+		String wordList = new String(Files.readAllBytes(Paths.get("src/hangman_project/"
+				+ "text_files/words.txt")), "UTF-8");
+		String[] wordArray = wordList.split(" ");
+		Random random = new Random();
+		word = wordArray[random.nextInt(wordArray.length -1)].replaceAll("[^a-zA-Z]", "").
+				toUpperCase().trim();
+		} catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
+		
+		return word;
+		
 	}
 
 }
